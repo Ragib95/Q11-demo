28d6c0404978c4fcbb352d1295471f71c9c10fc1
Ragib95 <dilnawazragib@gmail.com>
2018-06-26 11:17:34 +0530

    Demo signin and signup

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-southeast-1_cCzeLjAso', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3ilmjql8ilhu998sp6lpg8mgot', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://07s90sraq0.execute-api.us-east-1.amazonaws.com/dev/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
